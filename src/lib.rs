8f5bfa7f2bb1d04ea3eb3129b1d7f4f0ed6bc077
Cifko <gcifko@gmail.com>
2023-11-20 15:15:48 +0100

    feat: add common features

---
+++ src/lib.rs
@@ -0,0 +1,20 @@
+use tari_crypto::hash_domain;
+
+hash_domain!(
+    ConfidentialOutputHashDomain,
+    "com.tari.dan.confidential_output"
+);
+hash_domain!(TariDanConsensusHashDomain, "com.tari.dan.consensus");
+hash_domain!(TariEngineHashDomain, "com.tari.dan.engine");
+hash_domain!(
+    TransactionSecureNonceKdfDomain,
+    "com.tari.base_layer.core.transactions.secure_nonce_kdf"
+);
+hash_domain!(
+    ValidatorNodeBmtHashDomain,
+    "com.tari.base_layer.core.validator_node_mmr"
+);
+hash_domain!(
+    WalletOutputEncryptionKeysDomain,
+    "com.tari.base_layer.wallet.output_encryption_keys"
+);
